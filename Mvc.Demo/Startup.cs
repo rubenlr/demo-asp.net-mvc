95f99dc1bf5b8793dad3ede6ba5ae75cd132e19d
Rúben Lício Reis <rubenlr@gmail.com>
2016-04-13 17:11:21 -0300

    Inicial

---
+++ Mvc.Demo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Mvc.Demo.Startup))]
+namespace Mvc.Demo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
